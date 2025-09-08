dd6b72db54b41d4612f6a141d82ecfe347418685
Hyeonseong <127617182+hyeonseong1@users.noreply.github.com>
2025-09-08 16:07:18 +0900

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,79 @@
+# medium V1
+python main.py --env_name=antmaze-medium-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 0
+python main.py --env_name=antmaze-medium-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 1
+python main.py --env_name=antmaze-medium-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 2
+python main.py --env_name=antmaze-medium-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 3
+python main.py --env_name=antmaze-medium-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 4
+python main.py --env_name=antmaze-medium-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 5
+python main.py --env_name=antmaze-medium-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 6
+python main.py --env_name=antmaze-medium-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 7
+
+# medium V2
+python main.py --env_name=antmaze-medium-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 0
+python main.py --env_name=antmaze-medium-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 1
+python main.py --env_name=antmaze-medium-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 2
+python main.py --env_name=antmaze-medium-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 3
+python main.py --env_name=antmaze-medium-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 4
+python main.py --env_name=antmaze-medium-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 5
+python main.py --env_name=antmaze-medium-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 6
+python main.py --env_name=antmaze-medium-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 7
+
+# large V1
+python main.py --env_name=antmaze-large-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 0
+python main.py --env_name=antmaze-large-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 1
+python main.py --env_name=antmaze-large-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 2
+python main.py --env_name=antmaze-large-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 3
+python main.py --env_name=antmaze-large-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 4
+python main.py --env_name=antmaze-large-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 5
+python main.py --env_name=antmaze-large-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 6
+python main.py --env_name=antmaze-large-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 7
+
+# large V2
+python main.py --env_name=antmaze-large-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 0
+python main.py --env_name=antmaze-large-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 1
+python main.py --env_name=antmaze-large-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 2
+python main.py --env_name=antmaze-large-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 3
+python main.py --env_name=antmaze-large-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 4
+python main.py --env_name=antmaze-large-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 5
+python main.py --env_name=antmaze-large-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 6
+python main.py --env_name=antmaze-large-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 7
+
+# giant V1
+python main.py --env_name=antmaze-giant-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 0
+python main.py --env_name=antmaze-giant-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 1
+python main.py --env_name=antmaze-giant-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 2
+python main.py --env_name=antmaze-giant-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 3
+python main.py --env_name=antmaze-giant-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 4
+python main.py --env_name=antmaze-giant-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 5
+python main.py --env_name=antmaze-giant-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 6
+python main.py --env_name=antmaze-giant-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 7
+
+# giant V2
+python main.py --env_name=antmaze-giant-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 0
+python main.py --env_name=antmaze-giant-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 1
+python main.py --env_name=antmaze-giant-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 2
+python main.py --env_name=antmaze-giant-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 3
+python main.py --env_name=antmaze-giant-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 4
+python main.py --env_name=antmaze-giant-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 5
+python main.py --env_name=antmaze-giant-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 6
+python main.py --env_name=antmaze-giant-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 7
+
+# teleport V1
+python main.py --env_name=antmaze-teleport-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 0
+python main.py --env_name=antmaze-teleport-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 1
+python main.py --env_name=antmaze-teleport-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 2
+python main.py --env_name=antmaze-teleport-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 3
+python main.py --env_name=antmaze-teleport-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 4
+python main.py --env_name=antmaze-teleport-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 5
+python main.py --env_name=antmaze-teleport-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 6
+python main.py --env_name=antmaze-teleport-navigate-v0 --eval_episodes=50 --agent=agents/gcbc.py --seed 7
+
+# teleport V2
+python main.py --env_name=antmaze-teleport-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 0
+python main.py --env_name=antmaze-teleport-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 1
+python main.py --env_name=antmaze-teleport-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 2
+python main.py --env_name=antmaze-teleport-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 3
+python main.py --env_name=antmaze-teleport-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 4
+python main.py --env_name=antmaze-teleport-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 5
+python main.py --env_name=antmaze-teleport-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 6
+python main.py --env_name=antmaze-teleport-navigate-v0 --eval_episodes=50 --agent=agents/gcbcV2.py --seed 7
